fc6ff72161fcb3c106b7dd294ae5e1bf4c2605aa
Mircea Lungu <mircea.lungu@gmail.com>
2019-02-01 19:56:20 +0000

    ahem...

--- rebuild_api_core.sh
+++ rebuild_api_core.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-MICROSOFT_TRANSLATE_API_KEY='21281d0d5499487991f92351336da60b'
-GOOGLE_TRANSLATE_API_KEY='AIzaSyDCH0ikA5fzQiOaMlWzWi7HHY080-wZNqE'
-WORDNIK_API_KEY='b9588ee867984b055b14812a40f071e15a19e01d2e6362e15'
+MICROSOFT_TRANSLATE_API_KEY=''
+GOOGLE_TRANSLATE_API_KEY=''
+WORDNIK_API_KEY=''
 
 docker build -t zeeguu-api-core-dev -f docker-files-dev/zeeguu-api-core/Dockerfile .
 set +e
