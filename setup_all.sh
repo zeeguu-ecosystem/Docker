2ac9059469893ed4deadccfb61fe4c44437bfe34
Mircea Lungu <mircea.lungu@gmail.com>
2019-11-12 12:05:45 +0100

    adding aliases to /etc/hosts

--- setup_all.sh
+++ setup_all.sh
@@ -38,3 +38,18 @@ printf "http://rss.cnn.com/rss/edition_world.rss\nCNN World RSS Feed\ncnn.png\nC
 # German
 printf "http://newsfeed.zeit.de/sport/index\nGerman RSS Feed\ncnn.png\nGerman feed for sports\nde" | docker exec -i zeeguu-api-core python /opt/Zeeguu-API/tools/add_rssfeed.py
 docker exec -i zeeguu-api-core python /opt/Zeeguu-Core/tools/feed_retrieval.py
+
+echo "Updating your etc/hosts with the api.zeeguu.local and www.zeeguu.local entries."
+echo "You might be asked to provide your sudo password"
+echo ""
+
+echo '127.0.0.1 api.zeeguu.local' | sudo tee -a /etc/hosts
+echo '127.0.0.1 www.zeeguu.local' | sudo tee -a /etc/hosts
+
+echo ""
+echo "To try out the api you can "
+echo "    curl api.zeeguu.local/available_languages"
+echo " "
+
+echo "The website should be available at "
+echo "    www.zeeguu.local"
