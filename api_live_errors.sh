4263468a38583dea0aeebaae905574fc199e8713
Mircea Lungu <mircea.lungu@gmail.com>
2019-11-26 18:56:03 +0100

    quick deployment in containers works now for API and Web and Core

---
+++ api_live_errors.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+docker exec zeeguu-api-core-dev tail -f /var/log/apache2/error.log
--- prepare_for_develop.sh
+++ prepare_for_develop.sh
@@ -4,17 +4,23 @@ docker start zeeguu-mysql
 
 
 docker stop zeeguu-web
-docker rm -f zeeguu-web-dev > /dev/null 2>&1
-
-
-docker run --net=host -d --name=zeeguu-web-dev -v $(pwd)/Zeeguu-Web:/opt/Zeeguu-Web zeeguu-web
-docker exec zeeguu-web-dev python ./Zeeguu-Web/setup.py develop
-
-
 docker stop zeeguu-api-core
+
+docker rm -f zeeguu-web-dev > /dev/null 2>&1
 docker rm -f zeeguu-api-core-dev > /dev/null 2>&1
 
-docker run --net=host -d --name=zeeguu-api-core-dev -v $(pwd)/Zeeguu-API:/opt/Zeeguu-API zeeguu-api-core
-docker exec zeeguu-api-core-dev python ./setup.py develop
 
-echo "you can make changes to Zeeguu-Web & Zeeguu-API locally now"
+# Running the containers with the locally mapped folders
+docker run --net=host -d --name=zeeguu-api-core-dev \
+        -v $(pwd)/Zeeguu-API/zeeguu_api:/opt/Zeeguu-API/zeeguu_api \
+        -v $(pwd)/Zeeguu-Core/zeeguu_core:/opt/Zeeguu-Core/zeeguu_core \
+        zeeguu-api-core
+docker run --net=host -d --name=zeeguu-web-dev -v $(pwd)/Zeeguu-Web:/opt/Zeeguu-Web zeeguu-web
+
+# Uninstalling anything that was "installed" and deploying versions with "develop" instead
+docker exec zeeguu-api-core-dev python /opt/Zeeguu-Core/setup.py develop --uninstall
+docker exec zeeguu-api-core-dev python /opt/Zeeguu-API/setup.py develop --uninstall
+docker exec zeeguu-web-dev python /opt/Zeeguu-Web/setup.py develop --uninstall
+
+
+echo "After making local changes, make sure to run ./api_redeploy and ./web_redeploy "
