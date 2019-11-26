14af637fade46eb2523ec3e4a2e536cf1c5bbea3
Mircea Lungu <mircea.lungu@gmail.com>
2019-11-26 22:19:39 +0100

    api_redeploy works both from the host of a virtualbox or from the virtualbox itself

--- api_redeploy.sh
+++ api_redeploy.sh
@@ -4,5 +4,9 @@
 # have been deployed with setup.py develop.
 # corresponding lines int he docker-files/
 
-docker exec zeeguu-api-core-dev service apache2 reload 
+# if it fails to run the docker command, it assumes that
+# it might be called from the host machine, and thus 
+# 
+((echo "Trying to run with docker..." && docker exec zeeguu-api-core-dev service apache2 reload) \
+	|| (echo "Trying to run with vagrant..." && vagrant ssh -- -t 'docker exec zeeguu-api-core-dev service apache2 reload')) && echo "Success."
 
