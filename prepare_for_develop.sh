550ef3ddcdcc9e981d7f2caf814bd49b944b4e96
Mircea Lungu <mircea.lungu@gmail.com>
2019-11-26 14:36:15 +0100

    fix

--- prepare_for_develop.sh
+++ prepare_for_develop.sh
@@ -7,4 +7,7 @@ docker run --net=host -d --name=zeeguu-web-with-volume -v $(pwd)/Zeeguu-Web:/opt
 docker exec zeeguu-web-with-volume python ./Zeeguu-Web/setup.py develop
 
 
+docker start zeeguu-api-core
+docker start zeeguu-mysql
+
 echo "you can make changes to Zeeguu-Web locally now"
