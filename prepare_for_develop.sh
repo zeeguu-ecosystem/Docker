2710973e96131fcdcb9e26a481e4d9eb061b9705
mfurak <marek.furak@gmail.com>
2019-12-12 16:32:26 +0100

    Added mapping for dev deploy of Exercises, Reader, Dashboard

--- prepare_for_develop.sh
+++ prepare_for_develop.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+set -x
 
 docker start zeeguu-mysql
 
@@ -15,12 +16,26 @@ docker run --net=host -d --name=zeeguu-api-core-dev \
         -v $(pwd)/Zeeguu-API/zeeguu_api:/opt/Zeeguu-API/zeeguu_api \
         -v $(pwd)/Zeeguu-Core/zeeguu_core:/opt/Zeeguu-Core/zeeguu_core \
         zeeguu-api-core
-docker run --net=host -d --name=zeeguu-web-dev -v $(pwd)/Zeeguu-Web:/opt/Zeeguu-Web zeeguu-web
+docker run --net=host -d --name=zeeguu-web-dev \
+       -v $(pwd)/Zeeguu-Web:/opt/Zeeguu-Web \
+       -v $(pwd)/Zeeguu-Exercises:/opt/Zeeguu-Exercises \
+       -v $(pwd)/Zeeguu-Reader:/opt/Zeeguu-Reader \
+       -v $(pwd)/Zeeguu-Teacher-Dashboard:/opt/Zeeguu-Teacher-Dashboard \
+       -v $(pwd)/Zeeguu-Teacher-Dashboard-React:/opt/Zeeguu-Teacher-Dashboard-React \
+       zeeguu-web
 
 # Uninstalling anything that was "installed" and deploying versions with "develop" instead
 docker exec zeeguu-api-core-dev python /opt/Zeeguu-Core/setup.py develop --uninstall
 docker exec zeeguu-api-core-dev python /opt/Zeeguu-API/setup.py develop --uninstall
 docker exec zeeguu-web-dev python /opt/Zeeguu-Web/setup.py develop --uninstall
+docker exec zeeguu-web-dev python /opt/Zeeguu-Exercises/src/setup.py develop --uninstall
+docker exec zeeguu-web-dev python /opt/Zeeguu-Reader/src/setup.py develop --uninstall
+docker exec zeeguu-web-dev python /opt/Zeeguu-Teacher-Dashboard/src/setup.py develop --uninstall
+
+# Install the npm packages for the NodeJS projects
+docker exec zeeguu-web-dev bash -c "cd /opt/Zeeguu-Teacher-Dashboard-React/zeeguu-teacher-dashboard && npm install"
+docker exec zeeguu-web-dev bash -c "cd /opt/Zeeguu-Exercises && npm install"
+docker exec zeeguu-web-dev bash -c "cd /opt/Zeeguu-Reader && npm install"
 
 
 echo "After making local changes, make sure to run ./api_redeploy and ./web_redeploy "
