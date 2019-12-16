9966b6a3718b809fc9cca960d8c30a8b2b9a7332
mfurak <marek.furak@gmail.com>
2019-12-16 10:22:29 +0100

    Modified the dev scripts to separate one time setup functionality from every redeploy

--- prepare_for_develop.sh
+++ prepare_for_develop.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 set -x
+ORIG_TEACHER_DASHBOARD_DIR=`grep -i "Alias /teacher-dashboard" ./docker-files/zeeguu-web/apache-zeeguu.conf | awk '{ print $3 }'`
+NEW_TEACHER_DASHBOARD_DIR="/opt/Zeeguu-Teacher-Dashboard-React/zeeguu-teacher-dashboard/build"
 
 docker start zeeguu-mysql
 
@@ -32,10 +34,22 @@ docker exec zeeguu-web-dev python /opt/Zeeguu-Exercises/src/setup.py develop --u
 docker exec zeeguu-web-dev python /opt/Zeeguu-Reader/src/setup.py develop --uninstall
 docker exec zeeguu-web-dev python /opt/Zeeguu-Teacher-Dashboard/src/setup.py develop --uninstall
 
+#Start the python projects in develop mode
+docker exec zeeguu-web-dev python /opt/Zeeguu-Web/setup.py develop
+docker exec zeeguu-web-dev python /opt/Zeeguu-Exercises/src/setup.py develop
+docker exec zeeguu-web-dev python /opt/Zeeguu-Reader/src/setup.py develop
+docker exec zeeguu-web-dev python /opt/Zeeguu-Teacher-Dashboard/src/setup.py develop
+
 # Install the npm packages for the NodeJS projects
 docker exec zeeguu-web-dev bash -c "cd /opt/Zeeguu-Teacher-Dashboard-React/zeeguu-teacher-dashboard && npm install"
 docker exec zeeguu-web-dev bash -c "cd /opt/Zeeguu-Exercises && npm install"
 docker exec zeeguu-web-dev bash -c "cd /opt/Zeeguu-Reader && npm install"
+
+# Replace the directory of the Teacher Dashboard React project in the apache configuration
+docker exec -d zeeguu-web-dev sed -i "s,$ORIG_TEACHER_DASHBOARD_DIR,$NEW_TEACHER_DASHBOARD_DIR,g" /etc/apache2/sites-available/apache-zeeguu.conf
+ 
+# Reload the apache config
+docker exec zeeguu-api-core-dev service apache2 reload
 
 
 echo "After making local changes, make sure to run ./api_redeploy and ./web_redeploy "
--- web_redeploy.sh
+++ web_redeploy.sh
@@ -1,26 +1,11 @@
 #! /bin/bash
 set -x
 
-ORIG_TEACHER_DASHBOARD_DIR=`grep -i "Alias /teacher-dashboard" ./docker-files/zeeguu-web/apache-zeeguu.conf | awk '{ print $3 }'`
-NEW_TEACHER_DASHBOARD_DIR="/opt/Zeeguu-Teacher-Dashboard-React/zeeguu-teacher-dashboard/build"
-
 docker restart zeeguu-web-dev
 
-#Start the python projects in develop mode
-docker exec zeeguu-web-dev python /opt/Zeeguu-Web/setup.py develop
-docker exec zeeguu-web-dev python /opt/Zeeguu-Exercises/src/setup.py develop
-docker exec zeeguu-web-dev python /opt/Zeeguu-Reader/src/setup.py develop
-docker exec zeeguu-web-dev python /opt/Zeeguu-Teacher-Dashboard/src/setup.py develop
-
 # Start the NodeJS projects in develop mode
-docker exec -d zeeguu-web-dev bash -c "cd /opt/Zeeguu-Teacher-Dashboard-React/zeeguu-teacher-dashboard && npm run start"
+docker exec -d zeeguu-web-dev bash -c "cd /opt/Zeeguu-Teacher-Dashboard-React/zeeguu-teacher-dashboard && npm run dev"
 docker exec -d zeeguu-web-dev bash -c "cd /opt/Zeeguu-Exercises && npm run dev"
 docker exec -d zeeguu-web-dev bash -c "cd /opt/Zeeguu-Reader && npm run dev"
-
-# Replace the directory of the Teacher Dashboard React project in the apache configuration
-docker exec -d zeeguu-web-dev sed -i "s,$ORIG_TEACHER_DASHBOARD_DIR,$NEW_TEACHER_DASHBOARD_DIR,g" /etc/apache2/sites-available/apache-zeeguu.conf
- 
-# Reload the apache config
-docker exec zeeguu-api-core-dev service apache2 reload
 
 echo "Web is now redeployed and local changes are reflected at www.zeeguu.local"
