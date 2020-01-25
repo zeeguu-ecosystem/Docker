f9b0a0eade1015739501720a497e42d80c709f6b
Mircea Lungu <mircea.lungu@gmail.com>
2020-01-25 18:13:33 +0000

    new lineup; and starting the web from 3.7

--- start-all.sh
+++ start-all.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+export MICROSOFT_TRANSLATE_API_KEY='34624fe84ecb4843b0fee68f1b1915a2'
+export GOOGLE_TRANSLATE_API_KEY='AIzaSyAqpiMewmzdy0-nzt-PbdLaM4brOBzcx2w'
+export WORDNIK_API_KEY='1a4b8d8469e41f3bcf80b0b6177023a912d3db3eaa26c8893'
+export MULTI_LANG_TRANSLATOR_AB_TESTING=42
+
 docker rm -f zeeguu-mysql && docker run --restart=always --net=host -v /home/mlun/zeeguu-data/mysql:/var/lib/mysql -d --name=zeeguu-mysql zeeguu-mysql
 docker rm -f fmd-mysql && docker run -v /home/mlun/zeeguu-data/fmd-mysql:/var/lib/mysql -p 3307:3306 -d --name=fmd-mysql fmd-mysql
 docker rm -f zeeguu-api-core && docker run --restart=always --net=host -v /home/mlun/zeeguu-data:/opt/zeeguu-data -d -e MICROSOFT_TRANSLATE_API_KEY=$MICROSOFT_TRANSLATE_API_KEY -e GOOGLE_TRANSLATE_API_KEY=$GOOGLE_TRANSLATE_API_KEY -e WORDNIK_API_KEY=$WORDNIK_API_KEY -e MULTI_LANG_TRANSLATOR_AB_TESTING=$MULTI_LANG_TRANSLATOR_AB_TESTING  --name=zeeguu-api-core zeeguu-api-core
